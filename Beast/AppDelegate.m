eee3406e2229ae1339dad12806280da43addc6ac
Trevor Beasty <trevorbeasty@Trevors-iMac.local>
2016-12-10 16:13:10 -0500

    refine tab bar;

--- Beast/AppDelegate.m
+++ Beast/AppDelegate.m
@@ -40,47 +40,20 @@
     // root view controller
     
     TJBRealizedSetActiveEntryVC *vc1 = [[TJBRealizedSetActiveEntryVC alloc] init];
+    [vc1.tabBarItem setTitle: @"Active Entry"];
+    
     TJBRealizedSetHistoryByDay *vc2 = [[TJBRealizedSetHistoryByDay alloc] init];
+    [vc2.tabBarItem setTitle: @"Today's History"];
     
     UITabBarController *tbc = [[UITabBarController alloc] init];
     
     [tbc setViewControllers: @[vc1, vc2]];
+    tbc.tabBar.translucent = NO;
     
     self.window.rootViewController = tbc;
     
     return YES;
 }
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
-    [self.cdc saveContext];
-}
-
-
 
 
 @end
