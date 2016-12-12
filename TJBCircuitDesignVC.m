f4303254cbb068a46ea930a9751cad521beb5fc1
Trevor Beasty <trevorbeasty@Trevors-iMac.local>
2016-12-12 18:59:16 -0500

    dev of circuit configuration scene;

--- TJBCircuitDesignVC.m
+++ TJBCircuitDesignVC.m
@@ -10,10 +10,63 @@
 
 @interface TJBCircuitDesignVC ()
 
+{
+    double _numberOfExercises;
+    double _numberOfRounds;
+}
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *targetingWeightSC;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *targetingRepsSC;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *targetingRestSC;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *targetsVaryByRoundSC;
+
+@property (weak, nonatomic) IBOutlet UIStepper *numberOfExercisesStepper;
+@property (weak, nonatomic) IBOutlet UIStepper *numberOfRoundsStepper;
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOfExercisesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfRoundsLabel;
+
+
 @end
 
 @implementation TJBCircuitDesignVC
 
+#pragma mark - Instantiation
 
+- (void)viewDidLoad
+{
+    // steppers
+    
+    _numberOfExercises = 1.0;
+    _numberOfRounds = 1.0;
+    
+    self.numberOfExercisesLabel.text = [[NSNumber numberWithDouble: _numberOfExercises] stringValue];
+    self.numberOfRoundsLabel.text = [[NSNumber numberWithDouble: _numberOfRounds] stringValue];
+    
+    [self.numberOfExercisesStepper addTarget: self
+                                      action: @selector(didChangeExerciseStepperValue)
+                            forControlEvents: UIControlEventValueChanged];
+    [self.numberOfRoundsStepper addTarget: self
+                                   action: @selector(didChangeRoundsStepperValue)
+                         forControlEvents: UIControlEventValueChanged];
+}
+
+#pragma mark - Stepper Methods
+
+- (void)didChangeExerciseStepperValue
+{
+    double number = self.numberOfExercisesStepper.value;
+    
+    _numberOfExercises = number;
+    self.numberOfExercisesLabel.text = [[NSNumber numberWithDouble: number] stringValue];
+}
+
+- (void)didChangeRoundsStepperValue
+{
+    double number = self.numberOfRoundsStepper.value;
+    
+    _numberOfRounds = number;
+    self.numberOfRoundsLabel.text = [[NSNumber numberWithDouble: number] stringValue];
+}
 
 @end
