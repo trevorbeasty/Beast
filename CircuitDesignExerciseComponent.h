2f13fbfd7839a7cf6232749c7925d6df5d4e23b7
Trevor Beasty <trevorbeasty@Trevors-iMac.local>
2016-12-14 18:39:44 -0500

    continuation;

--- CircuitDesignExerciseComponent.h
+++ CircuitDesignExerciseComponent.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+// break it out.  Use xibs as much as possible cause adding constraints programmatically sucks ass.     One exercise component will consist of 2 classes - (1) a class that has the title bar and column labels and dynamically adds rows as needed (2) the class that constitutes a single row;  it's container view will be added as a subview to class in (1) and I will need to add programmatic constraints there.  (1) will also load 1 of 2 nibs based on whether or not targets vary across rounds;
+
 @interface CircuitDesignExerciseComponent : UIViewController
 
 @property (nonatomic, strong) NSNumber *numberOfRounds;
